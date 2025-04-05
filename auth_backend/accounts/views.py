212ef6d9b2c69dc7957bdb1f0a69a205a3ec1fd7
Maquiz1 <manquiz92@gmail.com>
2025-04-05 23:24:43 +0300

    fix back

---
+++ auth_backend/accounts/views.py
@@ -0,0 +1,28 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth.models import User
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
+
+class RegisterView(APIView):
+    def post(self, request):
+        username = request.data['username']
+        password = request.data['password']
+        if User.objects.filter(username=username).exists():
+            return Response({'error': 'User already exists'}, status=status.HTTP_400_BAD_REQUEST)
+        user = User.objects.create_user(username=username, password=password)
+        return Response({'message': 'User created successfully'}, status=status.HTTP_201_CREATED)
+
+class LoginView(APIView):
+    def post(self, request):
+        username = request.data['username']
+        password = request.data['password']
+        user = authenticate(username=username, password=password)
+        if user is not None:
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+            })
+        return Response({'error': 'Invalid Credentials'}, status=status.HTTP_401_UNAUTHORIZED)
